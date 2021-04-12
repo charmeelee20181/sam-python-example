3a061670cfa9095e385ed62ce127fa47dad0b684
charmeelee20181 <81811377+charmeelee20181@users.noreply.github.com>
2021-04-12 14:56:39 +0800

    Update create_stack.py

--- create_stack.py
+++ create_stack.py
@@ -7,7 +7,7 @@ def create_stack():
     Helper function to create a stack/changeset
     """
     try:
-        #Try creating stacks
+        #Try creating stack
         subprocess.run('aws cloudformation create-stack --stack-name sam-codebuild-full-iam --template-body file://cloudformation_template_full.yaml --capabilities CAPABILITY_NAMED_IAM',stdout=sys.stdout,stderr=sys.stdout,check=True)
     except subprocess.CalledProcessError as e:
         print("Stack already exists, creating changeset...")
